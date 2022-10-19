704c1e0d9a0dbfad77ea9512d523120031b6b21a
Srimped <103495935+Srimped@users.noreply.github.com>
2022-10-20 00:16:04 +0700

    Front-end Login, Register

---
+++ Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASM.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username too long!!")] 
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
