a1108c22008761df7756fe705b055ccbf4a2d4ef
qungmnh <106642260+qungmnh@users.noreply.github.com>
2022-10-26 15:34:30 +0700

    asd

--- Startup.cs
+++ Startup.cs
@@ -35,8 +35,8 @@ namespace ASM
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
-            });
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
